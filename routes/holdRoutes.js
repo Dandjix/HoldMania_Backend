d99ed89f49d4977ad5a22c62d6643b1eb7420300
Timon Dubreuil <timondubreuil@outlook.com>
2024-12-19 15:29:57 +0100

    ajout parametres a get holds

--- routes/holdRoutes.js
+++ routes/holdRoutes.js
@@ -4,34 +4,65 @@ const router = express.Router()
 const conn = require('../mysqlConnection')
 
 router.get('/',(req,res) => {
-    conn.query(
-        `
-        SELECT idHold,
+    try
+    {
+        const {idClientLevels,idColors} = req.query
 
-        HOLD_COLOR.holdColorName,
-        HOLD_TYPE.holdTypeName,
-        CLIENT_LEVEL.clientLevelName,
-
-        holdName,
-        price,
-        weight,
-        sizeMeters,
-        imageURL 
-
-        FROM HOLD
-        LEFT JOIN HOLD_COLOR ON HOLD.idHoldColor = HOLD_COLOR.idHoldColor
-        LEFT JOIN HOLD_TYPE ON HOLD.idHoldType = HOLD_TYPE.idHoldType
-        LEFT JOIN CLIENT_LEVEL ON HOLD.idClientLevel = CLIENT_LEVEL.idClientLevel
-        ;`
-        ,(err,result,fields) => {
-        if(err)
-        {
-            console.error("error : "+err)
-            res.status(500).json({error:err})
-            return
+        console.log(`query values : ${idClientLevels}|${idColors}`);
+        // query values : 1,2,3|1,2
+    
+        let whereConditions = [];
+        if (idClientLevels) {
+            const clientLevels = idClientLevels.split(',').map(level => parseInt(level, 10)).filter(Boolean);
+            if (clientLevels.length > 0) {
+                whereConditions.push(`HOLD.idClientLevel IN (${clientLevels.join(',')})`);
+            }
         }
-        res.send(result)
-    })
+
+        if (idColors) {
+            const colors = idColors.split(',').map(color => parseInt(color, 10)).filter(Boolean);
+            if (colors.length > 0) {
+                whereConditions.push(`HOLD.idHoldColor IN (${colors.join(',')})`);
+            }
+        }
+
+        const whereClause = whereConditions.length > 0 ? `WHERE ${whereConditions.join(' AND ')}` : '';
+
+
+        conn.query(
+            `
+            SELECT idHold,
+    
+            HOLD_COLOR.holdColorName,
+            HOLD_TYPE.holdTypeName,
+            CLIENT_LEVEL.clientLevelName,
+    
+            holdName,
+            price,
+            weight,
+            sizeMeters,
+            imageURL 
+    
+            FROM HOLD
+            LEFT JOIN HOLD_COLOR ON HOLD.idHoldColor = HOLD_COLOR.idHoldColor
+            LEFT JOIN HOLD_TYPE ON HOLD.idHoldType = HOLD_TYPE.idHoldType
+            LEFT JOIN CLIENT_LEVEL ON HOLD.idClientLevel = CLIENT_LEVEL.idClientLevel
+            ${whereClause};`
+            ,(err,result,fields) => {
+            if(err)
+            {
+                console.error("error : "+err)
+                res.status(500).json({error:err})
+                return
+            }
+            res.send(result)
+        })
+    }
+    catch(e)
+    {
+        res.status(500).json({error:"Internal error"})
+    }
+    
 })
 
 router.get('/:idHold',(req,res) => {
